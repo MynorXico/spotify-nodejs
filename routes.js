79eb5d9cc2b5022f872b589ecae3e987cab3a26c
Mynor Xico <xic14659@uvg.edu.gt>
2020-07-11 23:04:59 -0600

    Healthcheck endpoint

--- routes.js
+++ routes.js
@@ -4,5 +4,5 @@ var router = express.Router();
 var songController = require('./Controllers/SongController');
 
 router.get('/getPlaylistItems/:id', songController.test);
-
+router.get('/', (req, res) => {res.send({'api': 'songs'})});
 module.exports = router;
